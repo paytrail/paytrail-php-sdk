0c1ed9cae8c0b5d87157572fdf7f0627e3151c53
Joonas Loueranta <joonas.loueranta@paytrail.com>
2021-10-06 20:22:58 +0300

    Create response.php

---
+++ examples/payment/response.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * An example of a cart page.
+ */
+require 'vendor/autoload.php';
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Checkout Finland Payment Service Example</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <style> div {max-width: 1000px;}</style>
+</head>
+<body>
+    <div class="container-fluid">
+        <h1>Paytrail Payment Service Example</h1>
+        <?php
+        $status = $_GET["checkout-status"];
+        if ($status === 'ok') {
+            echo '<h2>Thank you for your purchase!</h2>';
+            echo '<div class="alert alert-success">Payment status: ' . $status . '</div>';
+        } else {
+            echo '<h4>There was a problem processing your request</h4>';
+            echo '<div class="alert alert-warning">Payment status: ' . $status . '</div>';
+        }
+        ?>
+        <form method="post" action="index.php">
+            <button type="submit" class="btn btn-primary">Continue shopping</button>
+        </form>
+    </div>
+</body>
