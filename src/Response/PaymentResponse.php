51e0aeeb92a66de10d8b2e774c7cd9d4707c75f9
Sakri Koskimies <sakri.koskimies@hotmail.com>
2022-10-24 21:17:44 +0300

    Update payment response model

--- src/Response/PaymentResponse.php
+++ src/Response/PaymentResponse.php
@@ -7,6 +7,7 @@ declare(strict_types=1);
 
 namespace Paytrail\SDK\Response;
 
+use Paytrail\SDK\Model\PaymentMethodGroup;
 use Paytrail\SDK\Model\Provider;
 use Paytrail\SDK\Interfaces\ResponseInterface;
 
@@ -21,18 +22,39 @@ class PaymentResponse implements ResponseInterface
 {
 
     /**
-     * The transaction id.
+     * Assigned transaction ID for the payment.
      *
      * @var string|null
      */
     protected $transactionId;
 
     /**
-     * Payment API url.
+     * URL to hosted payment gateway.
      *
      * @var string|null
      */
     protected $href;
+
+    /**
+     * Localized text with a link to the terms of payment.
+     *
+     * @var string|null
+     */
+    protected $terms;
+
+    /**
+     * Array of payment method group data with localized names and URLs to icons.
+     *
+     * @var PaymentMethodGroup[]
+     */
+    protected $groups = [];
+
+    /**
+     * The bank reference used for the payments.
+     *
+     * @var string|null
+     */
+    protected $reference;
 
     /**
      * Payment providers.
@@ -85,6 +107,69 @@ class PaymentResponse implements ResponseInterface
     public function setHref(?string $href) : PaymentResponse
     {
         $this->href = $href;
+
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getTerms(): ?string
+    {
+        return $this->terms;
+    }
+
+    /**
+     * @param string|null $terms
+     * @return PaymentResponse
+     */
+    public function setTerms(?string $terms): PaymentResponse
+    {
+        $this->terms = $terms;
+
+        return $this;
+    }
+
+    /**
+     * @return PaymentMethodGroup[]
+     */
+    public function getGroups(): array
+    {
+        return $this->groups;
+    }
+
+    /**
+     * @param PaymentMethodGroup[]|array $groups
+     * @return PaymentResponse
+     */
+    public function setGroups(array $groups): PaymentResponse
+    {
+        $this->groups = array_map(function ($group) {
+            if (! $group instanceof Provider) {
+                return (new Provider())->bindProperties($group);
+            }
+
+            return $group;
+        }, $groups);
+
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getReference(): ?string
+    {
+        return $this->reference;
+    }
+
+    /**
+     * @param string|null $reference
+     * @return PaymentResponse
+     */
+    public function setReference(?string $reference): PaymentResponse
+    {
+        $this->reference = $reference;
 
         return $this;
     }
