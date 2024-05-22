3adef5bdfdae470f14d594c57e804fa18092602c
Konrad Konieczny <51909244+konrad-konieczny@users.noreply.github.com>
2024-05-22 17:23:04 +0200

    remove negative rows validation

--- src/Model/Item.php
+++ src/Model/Item.php
@@ -379,9 +379,6 @@ class Item implements \JsonSerializable, ItemInterface
         if ($props['unitPrice'] === null) {
             throw new ValidationException('Item unitPrice is empty');
         }
-        if ($props['unitPrice'] < 0) {
-            throw new ValidationException('Items unitPrice can\'t be a negative number');
-        }
         if ($props['unitPrice'] > 99999999) {
             throw new ValidationException('Items unitPrice can\'t be over 99999999');
         }
